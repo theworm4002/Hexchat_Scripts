56615aeaf736b27f4fa327570a26897c96ce7709
theworm4002 <62223975+theworm4002@users.noreply.github.com>
2022-04-11 23:32:29 -0400

    to black

--- Mc_Slappy.py
+++ Mc_Slappy.py
@@ -8,30 +8,32 @@ __module_version__ = "1.0"
 __module_description__ = "A Slap plugin for HexChat"
 
 dirName = os.path.dirname(os.path.abspath(__file__))
-McSlappysActionFile = '{dirName}/slapActions.txt'.format(dirName)
+McSlappysActionFile = '{}/slapActions.txt'.format(dirName)
 
 def slap(word, word_eol, userdata):
     global McSlappysActions
     nick2Slap = word[1]
     
     msgpick = choice(McSlappysActions)
-    olSlappyCMD = msgpick.strip().format(nick2Slap)
+    nick2SlapBold = '{}'.format(nick2Slap)
+    olSlappyCMD = msgpick.strip().format(nick2SlapBold)
     hexchat.command(olSlappyCMD)
 
     return hexchat.EAT_ALL
  
-def reloadMcSlappy():
+def reloadMcSlappy(word, word_eol, userdata):
     global McSlappysActions
     #Using a file for Ct with love, Worm 
     with open(McSlappysActionFile, 'r') as fp:
         McSlappysActions = fp.readlines()
+    hexchat.prnt("McSlappysActions file loaded")
     return hexchat.EAT_ALL
 
 hexchat.hook_command('slap', slap, help="/slap <nick>")
 hexchat.hook_command('reloadMcSlappy', reloadMcSlappy, help="/reloadMcSlappy reloads McSlappy's Action File")
 
 try:
-    reloadMcSlappy()
+    reloadMcSlappy(1,2,3)
     hexchat.prnt(__module_name__ + " loaded.")
 except:
     hexchat.prnt("Failed to load")

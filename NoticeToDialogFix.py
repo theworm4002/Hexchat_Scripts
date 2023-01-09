22092a0cb07400d22bac1a74a22683c6d9ac6564
theworm4002 <62223975+theworm4002@users.noreply.github.com>
2023-01-09 12:41:38 -0500

    Added locops, wallops catch & option for server notice

--- NoticeToDialogFix.py
+++ NoticeToDialogFix.py
@@ -1,23 +1,56 @@
 # -*- coding: UTF-8 -*-
-import os
-import hexchat
-
-__module_name__ = "NoticeToDialogFix"
-__module_version__ = "0.0.1"
-__module_description__ = "Catches any NOTICE and makes it go to it's own Dialog window"
+# 
+#=============
+# = Settings =
+#============================================================
 
 # If you only want this to work on a single network
 networkName = 'Rizon'
+# Do you want server notices to go to  its own dialog window
+putServerNotice = False
+
+
+#============================================================
+import os
+import hexchat
+
+__module_name__ = 'NoticeToDialogFix'
+__module_version__ = '0.0.2'
+__module_description__ = 'Catches any NOTICE, LOCOPS, & WALLOPS, then makes it go to its own dialog window'
+
 
 def servNotice(word, word_eol, userdata):
-    if networkName != '' and hexchat.get_info("network") != networkName: return hexchat.EAT_NONE
+    if networkName != '' and hexchat.get_info('network') != networkName: return hexchat.EAT_NONE
     ircmsg = word_eol[0]
-    name = ircmsg.split('!',1)[0][1:]
-    msgSplit = ircmsg.split(' ',3)
-    target = msgSplit[2]
-    if target.startswith('#'): return hexchat.EAT_NONE
-    hexchat.command('query {}'.format(name))
-    return hexchat.EAT_NONE
 
+    if ' WALLOPS :' in ircmsg or ' LOCOPS :' in ircmsg:
+        # ':[Nick]!~[hostname]@[IPAddress] WALLOPS :[message]'
+        msgSplit = ircmsg.split(' ',2)
+        fullFrom = msgSplit[0]
+        fromName = fullFrom.split('!',1)[0][1:]
+        cmd = msgSplit[1]
+        if cmd != 'WALLOPS' or cmd != 'LOCOPS' : return hexchat.EAT_NONE        
+        hexchat.command('query {}'.format(fromName))
+        return hexchat.EAT_NONE        
 
-hexchat.hook_server("NOTICE", servNotice, priority=hexchat.PRI_HIGH)
+    elif ' NOTICE ' in ircmsg:
+        # ':[Nick]!~[hostname]@[IPAddress] NOTICE [channel] :[message]'
+        msgSplit = ircmsg.split(' ',3)
+        fullFrom = msgSplit[0]
+        try: 
+            fromName = fullFrom.split('!',1)[0][1:] 
+        except: 
+            if not putServerNotice: return hexchat.EAT_NONE
+            fromName = fullFrom
+        target = msgSplit[2]    
+        if target.startswith('#'): return hexchat.EAT_NONE        
+        hexchat.command('query {}'.format(fromName))
+        return hexchat.EAT_NONE
+
+    else:
+        return hexchat.EAT_NONE
+    
+
+hexchat.hook_server('RAW LINE', servNotice, priority=hexchat.PRI_HIGH)
+
+hexchat.prnt(__module_name__ + ' loaded.')
